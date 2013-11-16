3162cf7343bc91eeed733044774ff16e78d54c43
Jonathan Gawrych <Jonathan.Gawrych@gmail.com>
2013-11-15 20:59:49 -0700

    Slight fixes to angular's main.js (reverting a POST change) and making the controller loaded by the routeprovider rather than a directive

--- public/js/app.js
+++ public/js/app.js
@@ -6,25 +6,30 @@ angular.module('CityEventHub', ['ngRoute','ngResource'])
 .config(['$routeProvider','$locationProvider','$provide'
 ,function($routeProvider,  $locationProvider,  $provide) {
 
-	$routeProvider.when('/', {templateUrl: '/page/homepage'});
-	$routeProvider.when('/sandbox', {templateUrl: '/page/sandbox'});
-	$routeProvider.otherwise({templateUrl: '/partials/404'});
+	$routeProvider.when('/', {
+		templateUrl: '/page/homepage'
+	});
+
+	$routeProvider.when('/sandbox', {
+		templateUrl: '/page/sandbox',
+		controller: 'SandboxCtrl'
+	});
+	
+	$routeProvider.otherwise( {
+		templateUrl: '/partials/404'
+	});
+	
 	$locationProvider.html5Mode(true);
 
+
+	/////
 	// modify the $resource factory to return instances that have PUT defined as update,
 	// also fix POST so that it doesn't use the paramDefaults
 	var count = 0;
 	$provide.decorator('$resource',['$delegate',function($delegate) {
 		return function addUpdateMethod(url, paramDefaults, actions) {
 			// add update PUT method to $resource
-			var unParam = angular.copy(paramDefaults);
-			for (var param in unParam) {
-				if(unParam.hasOwnProperty(param)) {
-					unParam[param] = "";
-				}
-			}
-			actions = angular.extend({}, {update: {method: "PUT"},
-										  save:   {method: "POST", params: unParam}}, actions);
+			actions = angular.extend({}, {update: {method: "PUT"}}, actions);
 			return $delegate(url, paramDefaults, actions);
 		};
 	}]);
