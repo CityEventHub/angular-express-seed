f9250a7250fd2a7bb6ed1409c8bd1afe0bf0ebaa
Taft Sandbakken <taftsandbakken@gmail.com>
2013-11-30 13:55:55 -0700

    added schemas for events and profiles

--- routes/schemas.js
+++ routes/schemas.js
@@ -5,3 +5,30 @@ NamesSchema = mongoose.Schema({
 
 Name = mongoose.model('Name', NamesSchema);
 
+
+EventSchema = mongoose.Schema({ 
+  title: String,
+  location: String,
+  time: String,
+  date: Date,
+  description: String,
+  rank: Number,
+  rsvp: Number,
+  creator: String 
+});
+
+Event = mongoose.model('Event', EventSchema);
+
+
+ProfileSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  myEvents: [event],
+  upcomingEvents: [event],
+  settingDisplayInfo: Boolean,
+  settingShowRsvp: Boolean,
+  settingEmailMe: Boolean,
+  blacklisted: Boolean,
+});
+
+Profile = mongoose.model('Profile', ProfileSchema);
