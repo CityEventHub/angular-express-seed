e99b9f4a0ed40bae792b4f43812fa1d300853727
Jonathan Gawrych <Jonathan.Gawrych@gmail.com>
2013-12-06 11:24:35 -0700

    Removed duplicate event resource

--- public/js/resources/name-res.js
+++ public/js/resources/name-res.js
@@ -4,8 +4,5 @@
 angular.module('CityEventHub')
 .factory('Names', function($resource) {
 	return $resource('/api/names/:name', {name: '@_id'});
-})
-
-.factory('Events', function($resource) {
-	return $resource('/api/events/:event', {event: '@_id'});
 });
+
