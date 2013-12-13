142324506082bc8b3e790338c07e5c170ccd21aa
Sean Beck <seanmckaybeck@gmail.com>
2013-12-13 18:28:06 -0500

    Started adding in auth crap. Dont know how it all goes together. JavaScript is magical and weird

--- nodeapp.js
+++ nodeapp.js
@@ -31,7 +31,47 @@ app.use(express.methodOverride());
 app.use(express.static('public'));
 app.use(express.errorHandler());
 app.use(app.router);
+// passport
+var passport = require('passport')
+ , LocalStrategy = require('passport-local').Strategy;
 
+app.configure(function() {
+    app.use(express.static('public'));
+    app.use(express.cookieParser());
+    app.use(express.bodyParser());
+    app.use(express.session({secret: 'some secret'}));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(app.router);
+});
+
+// will change error messages once done to simply say incorrect creds, not specific to user or password
+passport.use(new LocalStrategy(
+    {usernameField: 'email'},
+    function(username, password, done) {
+        User.findOne({email : username }, function(err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username' });
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, {message: 'Incorrect password' });
+            }
+            return done(null, user);
+        });
+    }
+  ));
+
+// sorcery that passport says we need
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
 
 // serve JSON API
 require('./routes/api').load(app);
--- routes/api.js
+++ routes/api.js
@@ -49,6 +49,7 @@ require('./schemas.js');
 // see crud.js for more information
 var crud = require('./crud.js');
 
+var passport = require('passport');
 
 // Load the routes. we may want to break this up into subfunctions later.
 exports.load = function(app) {
@@ -64,6 +65,13 @@ exports.load = function(app) {
 	app.get('/api/names/:_id', crud.getDocument(Name), doMore);
 	app.put('/api/names/:_id', permissions, crud.putDocument(Name), doMore);
 	app.delete('/api/names/:_id', permissions, crud.deleteDocument(Name), doMore);
+
+        // Auth:
+        // QUESTION - HOW DO I GET THE PASSPORT VAR INTO HERE FROM nodeapp.js??????
+        app.post('/login', passport.authenticate('local', { successRedirect: '/',
+                                                             failureRedirect: '/login',
+                                                             failureFlash: true})
+        );
 
 	// Events:
 	app.get('/api/events', crud.getCollection(Event, true));
