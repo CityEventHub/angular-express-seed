9b3ce9ac70e007b97b4f6610217f5fecad46c226
Kevin Hinton <klhinton66@msn.com>
2013-12-17 11:03:17 -0700

    google map addition

--- public/js/controllers/events-ctrl.js
+++ public/js/controllers/events-ctrl.js
@@ -4,7 +4,10 @@
 angular.module('CityEventHub')
 .controller('EventsCtrl',['$scope','Users','Events','$routeParams','$rootScope',function($scope, Users, Events, $routeParams,$rootScope) {
 	
-	$scope.events = Events.query();
+	Events.query(function success(data) {
+		$scope.events = data;
+		$scope.putEventsOnMap();
+	});
 	$rootScope.activeTab = 1;
 	if($routeParams['search']){
 		$scope.searchString = $routeParams['search'];
@@ -35,5 +38,64 @@ angular.module('CityEventHub')
 	$scope.sendToEventPage = function(event){
 		window.location.href = 'event/' + event._id;
 	};
+
+	var map;
+	var geocoder;
+	var geocodeMap = [];
+
+	$scope.putEventsOnMap = function() {
+		for(var i = 0;i<$scope.events.length;i++){
+			(function(index) {
+				geocoder.geocode( { 'address': $scope.events[index].location}, function(results, status) {
+			      if (status == google.maps.GeocoderStatus.OK) {
+
+			      	var contentString = 
+			      	'<div id="content">'+
+					      '<h4 id="firstHeading" class="firstHeading">'+$scope.events[index].title+'</h4>'+
+					      '<div id="bodyContent">'+
+					      	'<p>'+$scope.events[index].description+'</p>'+
+					      '</div>'+
+				      '</div>';
+			        var infowindow = new google.maps.InfoWindow({
+					      content: contentString
+					  });
+			        if(index==0)
+			        	map.setCenter(results[0].geometry.location);
+			        var marker = new google.maps.Marker({
+			            map: map,
+			            position: results[0].geometry.location,
+			            title:$scope.events[index].title
+			        });
+			        google.maps.event.addListener(marker, 'click', function() {
+					    infowindow.open(map,marker);
+					});
+			      }
+			    },function(status){
+			    	console.log("Error",status);
+			    });
+			})(i);
+        }
+	}
+
+	$scope.initMaps  = function() {
+
+	    geocoder = new google.maps.Geocoder();
+	    var mapOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 13
+        };
+        map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+		geocoder.geocode( { 'address': "Provo,Ut"}, function(results, status) {
+	      if (status == google.maps.GeocoderStatus.OK) {
+	      	map.setCenter(results[0].geometry.location);
+	      } 
+	      else {
+	        alert("Geocode was not successful for the following reason: " + status);
+	      }
+	    });
+    }
+
+    $scope.initMaps();
 
 }]);
